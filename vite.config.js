a3b6587f377a65b55189e1bb0cc302c3a6ace0a7
jinseok1006 <jinseok1006@naver.com>
2022-12-25 02:08:01 +0900

    config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-usermanagement',
   server: {
     host: true,
     port: 9999,
